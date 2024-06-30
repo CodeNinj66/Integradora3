b5a82be88b4b5b055b6e112814d0ef0815576989
CodeNinj66 <cds5869@gmail.com>
2024-06-30 09:57:17 -0600

    Pantallas

---
+++ AplicacionPuntoDeVenta/client/src/app/components/principal-ventas/principal-ventas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-ventas',
+  templateUrl: './principal-ventas.component.html',
+  styleUrl: './principal-ventas.component.css'
+})
+export class PrincipalVentasComponent {
+generarTicket() {
+throw new Error('Method not implemented.');
+}
+
+}
